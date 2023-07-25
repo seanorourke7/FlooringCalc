6f1b8325570f0157fc751acdfde06aa413cfb093
seanorourke7 <>
2023-07-25 19:31:01 +0000

    add try except to all funtions for numerical input and def funtion to calculate total cost of room

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ def measurement_type():
     Determines if the user is using meters or feet 
     """
     while True:
-        print("Are you using Meters or Feet ? Enter m or f")
+        print("Are you using Meters or Feet ? Enter m or f \n")
         user_input = input()
     
         if validate_measurement_type(user_input):
@@ -21,7 +21,7 @@ def validate_measurement_type(values):
         return True
     try:
         if values != ('m' or 'f'):
-            raise TypeError(f"You Entered {values}. Only LowerCase m or f will be accepted")
+            raise TypeError(f"You Entered {values}. Only LowerCase m or f will be accepted \n")
     except TypeError as e:
         print(f"{e}. Please Try again")
         return False
@@ -33,19 +33,22 @@ user_input = measurement_type()
 
 
 def validate_num(values):
-    if values.isnumeric():
-        return True
+    """
+    Determines is the input is a number and raises an error if not. 
+    """
     try:
-        if type(values) != float or int:
-            raise ValueError(f"You Entered {values}. Only Numbers will be accepted")
+        if values.isnumeric():
+            return True
+        else:
+            raise ValueError(f"You Entered {values}. Only Numbers will be accepted \n")
     except ValueError as e:
         print(f"{e}. Please Try Again")
         return False
 
-    return True
-
-
 def calculate_length():
+    """
+    Take the input from the user for the Length
+    """
     while True:
         length = input("Please enter the length of the room \n")
         
@@ -56,7 +59,11 @@ def calculate_length():
 
 floor_length = calculate_length()
 
+
 def calculate_width():
+    """
+    Take the input from the user for the Width
+    """
     while True:
         width = input("Please enter the width of the room \n")
         
@@ -65,26 +72,49 @@ def calculate_width():
     return width
 
 
-width = calculate_width()
+floor_width = calculate_width()
 
-def calculate_boxes_needed():
-    box_size = float(input("Enter the SQM coverage of the box (in numbers only)\n"))
+  
+def box_coverage():
+    """
+    Take the input from the user to determine the coverage in each box.
+    """
+    while True:
+        box_size = input("Enter the SQM coverage of the box (in numbers only)\n")
+
+        if validate_num(box_size):
+            break
+    return box_size
+
+
+box_size = box_coverage()
+floor_size = (float(floor_length) * float(floor_width))
+
+def boxes_needed():
     if user_input == 'm':
-        boxes_required = floor / float(box_size)
+        boxes = (float(floor_size) / float(box_size))
     elif user_input == 'f':
-        boxes_required = (floor /10.764) / box_size
-    
-    print(f"You need {round(boxes_required)} boxes\n") 
-    return round(boxes_required)
-    
+        boxes = ((float(floor_size) /10.764) / float(box_size))
+        
+    return boxes
 
-boxes_needed = calculate_boxes_needed()
+    print(f"you need {boxes} boxes")
 
+
+boxes_needed = boxes_needed()
 
 def cost_of_room():
-    cost = float(input("Enter cost per box (numbers only leave out the € sign)\n"))
-    total_cost = boxes_needed * cost
-    print(f"The total cost will be €{total_cost}")
+    while True:
+        cost = input("Enter cost per box (numbers only leave out the € sign)\n")
+
+        if validate_num(cost):
+            break
+    return cost
+
+cost = cost_of_room()
+
+total_cost = (float(cost) * float(boxes_needed))
+print(f"The total cost will be €{(total_cost)}")
 
 
-cost_of_room()
+
