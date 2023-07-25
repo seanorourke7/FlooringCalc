a28df2d94da9683b83facfd920f36aaacf2a906a
seanorourke7 <>
2023-07-25 17:43:55 +0000

    add try except to validate numerical inputs from user

--- run.py
+++ run.py
@@ -1,17 +1,3 @@
-def validate_measurement_type(values):
-    """
-    Validates the measurement type prefered by the user
-    """
-    if values == 'm' or 'f':
-        return True
-    try:
-        if values != 'm' or 'f':
-            raise TypeError(f"You Entered {values}. Only LowerCase m or f will be accepted")
-    except TypeError as e:
-        print(f"{e}. Please Try again")
-        return False
-
-    return True
 
 def measurement_type():
     """
@@ -27,18 +13,59 @@ def measurement_type():
     return user_input
 
 
+def validate_measurement_type(values):
+    """
+    Validates the measurement type prefered by the user
+    """
+    if values == 'm' or values == 'f':
+        return True
+    try:
+        if values != ('m' or 'f'):
+            raise TypeError(f"You Entered {values}. Only LowerCase m or f will be accepted")
+    except TypeError as e:
+        print(f"{e}. Please Try again")
+        return False
+
+    return True
+
+
 user_input = measurement_type()
 
 
-def calculate_floor_size():
-    length = float(input("Please enter the length of the room \n"))
-    width = float(input("Please enter the width of the room\n"))
-    floor_size = length * width
-    print(f"The floor size is {floor_size} {user_input} Squared ")
-    return floor_size
+def validate_num(values):
+    if values.isnumeric():
+        return True
+    try:
+        if type(values) != float or int:
+            raise ValueError(f"You Entered {values}. Only Numbers will be accepted")
+    except ValueError as e:
+        print(f"{e}. Please Try Again")
+        return False
 
-floor = calculate_floor_size()
+    return True
 
+
+def calculate_length():
+    while True:
+        length = input("Please enter the length of the room \n")
+        
+        if validate_num(length):
+            break
+    return length
+
+
+floor_length = calculate_length()
+
+def calculate_width():
+    while True:
+        width = input("Please enter the width of the room \n")
+        
+        if validate_num(width):
+            break
+    return width
+
+
+width = calculate_width()
 
 def calculate_boxes_needed():
     box_size = float(input("Enter the SQM coverage of the box (in numbers only)\n"))
