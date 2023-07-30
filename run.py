c829aa27acb8886eff96c69ae560fb29b39652d9
seanorourke7 <>
2023-07-30 17:56:20 +0000

    add more descriptive text to funtions and clean up grammer in  inputs

--- run.py
+++ run.py
@@ -1,22 +1,13 @@
 import math
 
 
-def welcome():
-    """
-    Prints Welcome Message and explains how to use.
-    """
-    print("Welcome to Sean's Floor measuring tool\n")
-    print("Please follow the input instructions carefully")
-
-
 def measurement_type():
     """
     Determines if the user is using meters or feet
     """
     while True:
 
-        print()
-        print("Are you using Meters or Feet ? Enter m or f \n")
+        print("Are you using Meters or Feet ? Enter m or f. \n")
         user_input = input()
         if validate_measurement_type(user_input):
             break
@@ -32,9 +23,9 @@ def validate_measurement_type(values):
         return True
     try:
         if values != ('m' or 'f'):
-            raise TypeError(f"You Entered {values}. \n")
+            raise TypeError(f"You Entered {values}.\n")
     except TypeError as e:
-        print(f"{e}. Only LowerCase m or f will be accepted. Please Try again")
+        print(f"{e}Only LowerCase m or f will be accepted. Please Try again")
         return False
 
     return True
@@ -44,11 +35,13 @@ def validate_num(values):
     """
     Determines is the input is a number and raises an error if not.
     """
+    if values.replace(".", "").isnumeric():
+        return True
     try:
-        if not float(values):
+        if values is not float:
             raise ValueError(f"You Entered {values}. \n")
     except ValueError as e:
-        print(f"{e}. Please Enter a Number")
+        print(f"{e}Please Enter a Number\n")
         return False
 
     return True
@@ -59,7 +52,7 @@ def calculate_length():
     Take the input from the user for the Length
     """
     while True:
-        length = input("Please enter the length of the room \n")
+        length = input("Please enter the length of the room. (numbers only)\n")
         if validate_num(length):
             break
     return length
@@ -70,7 +63,7 @@ def calculate_width():
     Take the input from the user for the Width
     """
     while True:
-        width = input("Please enter the width of the room \n")
+        width = input("Please enter the width of the room. (numbers only)\n")
         if validate_num(width):
             break
     return width
@@ -78,10 +71,10 @@ def calculate_width():
 
 def box_coverage():
     """
-    Take the input from the user to determine the coverage in each box.
+    Take the input from the user to determine the square meter coverage in each box.
     """
     while True:
-        box_size = input("Enter the Square Meter coverage of the box\n")
+        box_size = input("Enter the Square Meter coverage of the box. (numbers only)\n")
 
         if validate_num(box_size):
             break
@@ -99,7 +92,7 @@ def calc_boxes_needed():
 
     elif user_input == 'f':
         boxes = ((float(floor_size) / 10.764) / float(box_size))
-    print(f"you need {math.ceil(boxes)} boxes")
+    print(f"you need {math.ceil(boxes)} boxes.")
 
     return math.ceil(boxes)
 
@@ -109,7 +102,7 @@ def cost_per_box():
     Takes the input from the user for the price of each box.
     """
     while True:
-        cost_of_box = input("Enter cost per box (numbers only)\n")
+        cost_of_box = input("Enter cost per box (numbers only).\n")
 
         if validate_num(cost_of_box):
             break
@@ -117,20 +110,12 @@ def cost_per_box():
 
 
 def calc_total_cost():
+    """
+    Calculates the Total cost before any offers are applied
+    """
     total_cost = (float(cost_of_box) * float(boxes))
     print('')
-    print(f"The total cost will be €{(total_cost)}")
-
-
-welcome()
-user_input = measurement_type()
-floor_length = calculate_length()
-floor_width = calculate_width()
-box_size = box_coverage()
-floor_size = (float(floor_length) * float(floor_width))
-boxes = calc_boxes_needed()
-cost_of_box = cost_per_box()
-calc_total_cost()
+    print(f"The total cost will be €{(total_cost)}.\n")
 
 
 def offer_input():
@@ -140,8 +125,9 @@ def offer_input():
     while True:
         print('')
         print("Is there currently an offer on ?\n")
-        print("Enter 1 to select '3 for 2', Enter 2 for '4 for 3'\n")
-        offer = input("Or Enter 3 to exit\n")
+        print("Enter 1 to select '3 for 2'.\n") 
+        print("Enter 2 to select '4 for 3'.\n")
+        offer = input("Or Enter 3 to exit.\n")
 
         if validate_offer_type(offer):
             break
@@ -150,7 +136,7 @@ def offer_input():
 
 def validate_offer_type(values):
     """
-    Validates the offer type prefered by the user
+    Validates the input from the user to determine the offer type
     """
     if values == '1' or values == '2' or values == '3':
         return True
@@ -158,7 +144,7 @@ def validate_offer_type(values):
         if values != ('1' or '2' or '3'):
             raise TypeError(f"You Entered {values}. \n")
     except TypeError as e:
-        print(f"{e}. Only 1, 2 or 3 will be accepted. Please Try again")
+        print(f"{e}. Only 1, 2 or 3 will be accepted. Please Try again.\n")
         return False
 
     return True
@@ -166,24 +152,39 @@ def validate_offer_type(values):
 
 def calc_3_for_2():
     """
-    Calculates the cost with a 3 for 2 offer.
+    Calculates the cost with a 3 for 2 offer. By dividing by 3 and multiplying 
+    by 2 and allowing for the remainder to be added as well
+    to avoid fractions of boxes.
     """
     if offer == '1':
         r = boxes % 3
         offer_price = ((math.floor((boxes / 3)) * 2) + r) * float(cost_of_box)
-        print(f"the price with 3 for 2 applied is €{math.ceil(offer_price)}\n")
+        print(f"The price with '3 for 2' is €{math.ceil(offer_price)}.\n")
 
 
 def calc_4_for_3():
     """
-    Calculates the cost with a 4 for 3 offer.
+    Calculates the cost with a 4 for 3 offer.By dividing by 4 and multiplying 
+    by 3 and allowing for the remainder to be added as well
+    to avoid fractions of boxes.
     """
     if offer == '2':
         r = boxes % 4
         offer_price = ((math.floor((boxes / 4)) * 3) + r) * float(cost_of_box)
-        print(f"the price with 4 for 3 applied is €{math.ceil(offer_price)}\n")
+        print(f"The price with '4 for 3' is €{math.ceil(offer_price)}.\n")
 
 
+print("Welcome to Sean's Floor measuring tool.\n")
+print("Please follow the input instructions carefully.\n")
+
+user_input = measurement_type()
+floor_length = calculate_length()
+floor_width = calculate_width()
+box_size = box_coverage()
+floor_size = (float(floor_length) * float(floor_width))
+boxes = calc_boxes_needed()
+cost_of_box = cost_per_box()
+calc_total_cost()
 offer = offer_input()
 calc_3_for_2()
 calc_4_for_3()
